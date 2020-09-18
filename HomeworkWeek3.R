91c4d97e4b823bc3aa5e6d7b9b018636958ad397
sokin-ili <sokiniliopoulos@gmail.com>
2020-09-18 23:49:06 +0300

    Create HomeworkWeek3.R

---
+++ HomeworkWeek3.R
@@ -0,0 +1,28 @@
+makeCacheMatrix <- function(x = matrix) {
+  m <- NULL 
+  set <- function(y) {
+    x <<- y
+    m <<- NULL 
+  }
+  get <- function() x 
+  setInv <- function(inverse) m <<- inverse
+  getInv <- function() m 
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getInv() 
+  if(!is.null(m)) {
+    message('getting cached data')
+    return(m)
+  }
+  
+  data <- x$get() 
+  m <- solve(data, ...) 
+  x$setInv(m) 
+  m 
+}
+
+
+
+
